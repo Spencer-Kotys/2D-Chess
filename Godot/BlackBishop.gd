d6fc10a409928fe309702d227c644b605169d11f
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2023-01-02 15:12:30 -0600

    Changing mouse click to image

--- Godot/BlackBishop.gd
+++ Godot/BlackBishop.gd
@@ -4,15 +4,15 @@ var grabbed = false
 var mouse_offset = Vector2()
 var prev_pos = Vector2()
 
-func _on_KinematicBody2D_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if not grabbed:
-			prev_pos = position
-		grabbed = event.pressed
-		mouse_offset = position - get_global_mouse_position()
-
 func _process(delta):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and grabbed:
 		position = get_global_mouse_position() + mouse_offset
 	if Input.is_physical_key_pressed(KEY_ENTER):
 		position = prev_pos
+
+func _on_Image_gui_input(event):
+	if event is InputEventMouseButton:
+		if not grabbed:
+			prev_pos = position
+		grabbed = event.pressed
+		mouse_offset = position - get_global_mouse_position()
