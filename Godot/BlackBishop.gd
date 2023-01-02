97e143368510306e0337d424fb3a8ec252ff6e6e
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2023-01-02 16:01:18 -0600

    Test snapping to grid position

--- Godot/BlackBishop.gd
+++ Godot/BlackBishop.gd
@@ -3,12 +3,15 @@ extends Sprite
 var grabbed = false
 var mouse_offset = Vector2()
 var prev_pos = Vector2()
+var new_pos = Vector2()
 
 func _process(delta):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and grabbed:
 		position = get_global_mouse_position() + mouse_offset
 	if Input.is_physical_key_pressed(KEY_ENTER):
 		position = prev_pos
+	if not grabbed:
+		position = new_pos
 
 func _on_Image_gui_input(event):
 	if event is InputEventMouseButton:
@@ -20,3 +23,5 @@ func _on_Image_gui_input(event):
 
 func _on_Area2D_area_entered(area):
 	print(area.name)
+	print(area.position)
+	new_pos = area.position
